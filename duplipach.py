a288ebbf610a705c5035e0e61a4e66d55efba286
Karryanna <karry@karryanna.cz>
2020-06-02 11:44:04 +0200

    Porovnání dvojic souborů a clusterování podle hešů

--- duplipach.py
+++ duplipach.py
@@ -1,12 +1,60 @@
 #!/usr/bin/env python3
 
+import filecmp
 import hashlib
+import queue
 
-def checksum(filename, blocksize=65536, as_hex=False):
+from collections import defaultdict
+from pathlib import Path
+
+def checksum(filename:str, blocksize:int=65536, as_hex:bool=False):
   hash_fun = hashlib.md5()
   with open(filename, 'rb') as infile:
     for chunk in iter(lambda: infile.read(blocksize), b''):
         hash_fun.update(chunk)
   return hash_fun.hexdigest() if as_hex else hash_fun.digest()
-  return hash_fun.hexdigest()
-  #return hash_fun.digest()
+
+def get_all_files(base_path):
+  path_queue = queue.Queue()
+  path_queue.put(Path(base_path))
+  while not (path_queue.empty()):
+    cur_path = path_queue.get()
+    if cur_path.is_dir():
+      for p in cur_path.iterdir():
+        path_queue.put(p)
+    elif cur_path.is_file():
+      yield cur_path
+
+def check_pair(file1, file2):
+  return filecmp.cmp(file1, file2, shallow=False)
+
+def cluster_by_hashes(base_path):
+  hashes = defaultdict(set)
+  for f in get_all_files(base_path):
+    file_hash = checksum(f)
+    hashes[file_hash].add(f)
+  return hashes
+
+def cluster_keys_as_hex(hashes):
+  bin_keys = list(hashes.keys())
+  for k in bin_keys:
+    hashes[k.hex()] = hashes[k]
+    del(hashes[k])
+
+if __name__ == '__main__':
+  import argparse
+
+  argparser = argparse.ArgumentParser()
+  argparser.add_argument('--list')
+  argparser.add_argument('dir')
+  argparser.add_argument('--report-all', dest='duplicates_only', action='store_false');
+  argparser.add_argument('--duplicates-only', action='store_true')
+  args = argparser.parse_args()
+
+  if args.list:
+    hashes = cluster_by_hashes(args.dir)
+    cluster_keys_as_hex(hashes)
+    with open(args.list, 'wt', encoding='utf-8') as outfile:
+      for hash, files in sorted(hashes.items()):
+        if ((not args.duplicates_only) or (len(files) > 1)):
+          outfile.write('{}: {}\n'.format(hash, ', '.join((str(f) for f in files))))
--- test_duplipach.py
+++ test_duplipach.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+from collections import defaultdict
+from pathlib import Path
+
 import pytest
 
 import duplipach
@@ -24,3 +27,84 @@ def test_checksum__large(tmp_path):
     for i in range(3 * 10**7):
       outfile.write('Toto je jen další zcela zbytečný řádek textu.\n')
   assert duplipach.checksum(filename, as_hex=True) == '1bf7313dc1136c7729f08f48c462c96b'
+
+def test_check_pair__same(tmp_path):
+  f1 = tmp_path / 'a'
+  f2 = tmp_path / 'b'
+  txt = 'Ahoj světe'
+  with open(f1, 'wt', encoding='utf-8') as outfile:
+    outfile.write(txt)
+  with open(f2, 'wt', encoding='utf-8') as outfile:
+    outfile.write(txt)
+  assert duplipach.check_pair(f1, f2) == True
+
+def test_check_pair__different(tmp_path):
+  f1 = tmp_path / 'a'
+  f2 = tmp_path / 'b'
+  txt1 = 'Ahoj světe'
+  txt2 = 'Ahoj lidi'
+  with open(f1, 'wt', encoding='utf-8') as outfile:
+    outfile.write(txt1)
+  with open(f2, 'wt', encoding='utf-8') as outfile:
+    outfile.write(txt2)
+  assert duplipach.check_pair(f1, f2) == False
+
+def test_cluster_by_hashes(tmp_path):
+  f1 = tmp_path / 'a'
+  f2 = tmp_path / 'b'
+  f3 = tmp_path / 'c'
+  f4 = tmp_path / 'd'
+  txt1 = 'Ahoj světe\n'
+  txt2 = 'Čau lidi\n'
+  txt3 = 'Páčko\n'
+
+  with open(f1, 'wt', encoding='utf-8') as outfile:
+    outfile.write(txt1)
+  with open(f2, 'wt', encoding='utf-8') as outfile:
+    outfile.write(txt2)
+  with open(f3, 'wt', encoding='utf-8') as outfile:
+    outfile.write(txt3)
+  with open(f4, 'wt', encoding='utf-8') as outfile:
+    outfile.write(txt2)
+
+  expected = defaultdict(set)
+  expected[b'\x98\xe8\xe2a\x8a3\xceU\xc1S\x98\x90\x91\xe3\xcb9'].add(Path(tmp_path / 'a'))
+  expected[b'\xeb\xd3C\x0b^\xbf\xe0_\x13(\x04L\xeb}\xa2\xf8'].add(Path(tmp_path / 'b'))
+  expected[b'5\x02\x03\xfd\xa5.\xe5\x9a\xb9F\x1f\x0f\xcc\x86\xe6\xf7'].add(Path(tmp_path / 'c'))
+  expected[b'\xeb\xd3C\x0b^\xbf\xe0_\x13(\x04L\xeb}\xa2\xf8'].add(Path(tmp_path / 'd'))
+
+  assert duplipach.cluster_by_hashes(tmp_path) == expected
+
+def test_cluster_keys_as_hex(tmp_path):
+  f1 = tmp_path / 'a'
+  f2 = tmp_path / 'b'
+  f3 = tmp_path / 'c'
+  f4 = tmp_path / 'd'
+  txt1 = 'Ahoj světe\n'
+  txt2 = 'Čau lidi\n'
+  txt3 = 'Páčko\n'
+
+  with open(f1, 'wt', encoding='utf-8') as outfile:
+    outfile.write(txt1)
+  with open(f2, 'wt', encoding='utf-8') as outfile:
+    outfile.write(txt2)
+  with open(f3, 'wt', encoding='utf-8') as outfile:
+    outfile.write(txt3)
+  with open(f4, 'wt', encoding='utf-8') as outfile:
+    outfile.write(txt2)
+
+  provided = defaultdict(set)
+  expected = defaultdict(set)
+
+  provided[b'\x98\xe8\xe2a\x8a3\xceU\xc1S\x98\x90\x91\xe3\xcb9'].add(Path(tmp_path / 'a'))
+  provided[b'\xeb\xd3C\x0b^\xbf\xe0_\x13(\x04L\xeb}\xa2\xf8'].add(Path(tmp_path / 'b'))
+  provided[b'5\x02\x03\xfd\xa5.\xe5\x9a\xb9F\x1f\x0f\xcc\x86\xe6\xf7'].add(Path(tmp_path / 'c'))
+  provided[b'\xeb\xd3C\x0b^\xbf\xe0_\x13(\x04L\xeb}\xa2\xf8'].add(Path(tmp_path / 'd'))
+
+  expected['98e8e2618a33ce55c153989091e3cb39'].add(Path(tmp_path / 'a'))
+  expected['ebd3430b5ebfe05f1328044ceb7da2f8'].add(Path(tmp_path / 'b'))
+  expected['350203fda52ee59ab9461f0fcc86e6f7'].add(Path(tmp_path / 'c'))
+  expected['ebd3430b5ebfe05f1328044ceb7da2f8'].add(Path(tmp_path / 'd'))
+
+  duplipach.cluster_keys_as_hex(provided)
+  assert provided == expected
